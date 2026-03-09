b005443a86841ae6b45fc372c6c1330028e6f08d
omkarfadanvis <omkar.d.fadanvis1515@gmail.com>
2026-03-09 22:07:05 +0530

    Update HackOnPartners.js

--- src/components/Hackthon/HackOnPartners.js
+++ src/components/Hackthon/HackOnPartners.js
@@ -3,18 +3,7 @@ import React from "react";
 const HackOnPartners = () => {
   return (
     <>
-    <div className="w-full flex flex-col gap-5 p-5">
-      <h3 className="font-medium text-white text-3xl md:py-5 md:text-5xl text-center">
-        Title Sponsor
-      </h3>
-
    
-      <div className="flex justify-center items-center gap-x-20 gap-y-10 flex-wrap">
-        <img src="/sponsor/Grras Nagpur Logo (1).png" alt="Grras Nagpur" className="w-3/4 h-32 md:w-auto max-w-[250px] md:max-w-[300px]" />
-        
-        {/* <img src="/sponsor/vikalp.png" alt="Sponsor 3" className="w-3/4 h-36 md:w-auto max-w-[250px] md:max-w-[300px]" /> */}
-      </div>
-    </div>
     <div className="w-full flex flex-col gap-5 p-5">
       <h3 className="font-medium text-white text-3xl md:py-5 md:text-5xl text-center">
         Powered By
